bb8620a21a6c3b4026951b854925d2f7a21d05eb
Emil Yordanov <emil.iordanov835@gmail.com>
2022-03-03 16:44:19 +0200

    Added list with foods in Test Balanced Diet View and implemented logic for it into DietController.

--- DietCalculatorSystem/Controllers/DietController.cs
+++ DietCalculatorSystem/Controllers/DietController.cs
@@ -1,14 +1,60 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DietCalculatorSystem.Data;
+using DietCalculatorSystem.Models.Foods;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace DietCalculatorSystem.Controllers
 {
     public class DietController : Controller
     {
-        [Authorize]
-        public IActionResult Balanced()
+        private readonly DietCalculatorDbContext data;
+
+        public DietController(DietCalculatorDbContext data)
         {
-            return View();
+            this.data = data;
+        }
+
+        [Authorize]
+        public IActionResult Balanced([FromQuery] AllFoodsQueryModel query)
+        {
+            var foodsAsQuery = data.Foods.AsQueryable();
+
+
+            if (!string.IsNullOrWhiteSpace(query.SearchTerm))
+            {
+                foodsAsQuery = foodsAsQuery
+                    .Where(x => x.Name.ToLower().Contains(query.SearchTerm.ToLower()));
+            }
+
+            int totalFoods = foodsAsQuery.Count();
+
+            foodsAsQuery = query.Sorting switch
+            {
+                FoodSorting.Proteins => foodsAsQuery.OrderByDescending(x => x.Proteins),
+                FoodSorting.Fats => foodsAsQuery.OrderByDescending(x => x.Fats),
+                FoodSorting.Carbohydrates => foodsAsQuery.OrderByDescending(x => x.Carbohydrates),
+                _ => foodsAsQuery.OrderByDescending(x => x.Calories),
+            };
+
+            query.Foods = foodsAsQuery
+                .Skip((query.CurrentPage - 1) * AllFoodsQueryModel.FoodsPerPage)
+                .Take(AllFoodsQueryModel.FoodsPerPage)
+                .Select(x => new AllFoodsFormModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    PictureUrl = x.PictureUrl,
+                    Calories = x.Calories,
+                    Proteins = x.Proteins,
+                    Fats = x.Fats,
+                    Carbohydrates = x.Carbohydrates,
+                })
+                .ToList();
+
+            query.TotalFoods = totalFoods;
+
+            return View(query);
         }
     }
 }
