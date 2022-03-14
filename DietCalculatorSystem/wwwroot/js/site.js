625025d6362dc6fedc47c7b6456f52dd5299ae16
Emil Yordanov <emil.iordanov835@gmail.com>
2022-03-14 19:57:52 +0200

    Added expanding button for all the diets.

--- DietCalculatorSystem/wwwroot/js/site.js
+++ DietCalculatorSystem/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$("[data-toggle='toggle']").click(function () {
+    var selector = $(this).data("target");
+    $(selector).toggleClass('in');
+});
