314f5587e9f56806509b2ff08fa91b5e88f04ffe
Emil Yordanov <emil.iordanov835@gmail.com>
2022-04-03 09:49:03 +0300

    Added design to Calculator and Add Food pages.

--- DietCalculatorSystem/Models/Home/CalculateCaloriesFormModel.cs
+++ DietCalculatorSystem/Models/Home/CalculateCaloriesFormModel.cs
@@ -6,22 +6,22 @@ namespace DietCalculatorSystem.Models.Home
 {
     public class CalculateCaloriesFormModel
     {
-        [Required(ErrorMessage = "Age is required")]
+        [Required(ErrorMessage = "Age is required.")]
         [Range(MinAge,MaxAge)]
         public int? Age { get; set; }
 
-        [Required(ErrorMessage = "Gender is required")]
+        [Required(ErrorMessage = "Gender is required.")]
         public string Gender { get; set; }
 
-        [Required(ErrorMessage = "Height is required")]
+        [Required(ErrorMessage = "Height is required.")]
         [Range(MinHeight,MaxHeight)]
         public double? Height { get; set; }
 
-        [Required(ErrorMessage = "Weight is required")]
+        [Required(ErrorMessage = "Weight is required.")]
         [Range(MinWeight,MaxWeight)]
         public double? Weight { get; set; }
 
-        [Required(ErrorMessage = "Activity is required")]
+        [Required(ErrorMessage = "Activity is required.")]
         public double? Activity { get; set; }
     }
 }
